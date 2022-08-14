d5b909595bedc47fd78691d5d45943439f6a2564
See-Its-Manish <seeitsmanish@gmail.com>
2022-08-14 13:55:01 +0530

    Files Added

---
+++ dataTypes.py
@@ -0,0 +1,59 @@
+a = 4
+b = 5
+print(type(a))
+print(type(a/b))	# Implicit Typecasting/Promotion
+print(int(a/b))     # Expliciting Typecasting
+
+print()
+
+#int
+
+a = 12
+b = 23
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a//b)
+
+print()
+
+#float
+c = 12.5
+print(type(c))
+a = 4
+print("type of a : ",type(a))
+print("type of a + c", type(a+c))
+
+
+print()
+
+
+#complex number
+d = 4 + 3j
+print(type(d))
+print(d)
+e = 5+7j
+print(d+e, d-e)
+
+print()
+
+
+#strings
+s1 = 'Manish'
+s2= "Parleen"
+print(s1)
+print(s2)
+
+print(s1 + s2)
+# print(s1 - s2) # Is this possible - No
+print(s1[2])
+print(s1[0:3])
+
+
+
+
+
+
+
+
---
+++ printStatement.py
@@ -0,0 +1,17 @@
+# end
+
+print("Parleen", end = "$")
+print("Kaur")
+
+
+
+# sep
+
+print("Parleen", "Kaur", "Arora", sep ="#")
+
+
+print(12 + 13)
+
+print("12" + "13")
+
+print(12 + "12")
